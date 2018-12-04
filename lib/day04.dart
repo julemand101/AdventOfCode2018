b4c80de641074de580cd50d9bfa3d9a29d1d7a73
Jacob Bang <julemand101@archlinux.dk>
2018-12-04 08:35:04 +0100

    Day04: Solved

---
+++ lib/day04.dart
@@ -0,0 +1,102 @@
+// --- Day 4: Repose Record ---
+// https://adventofcode.com/2018/day/4
+
+class Guard {
+  final int id;
+
+  // Key = minute, value = how often the guard sleeps on this minute
+  final Map<int, int> sleepMap = {};
+
+  Guard(this.id);
+
+  DateTime _startSleepTime;
+
+  void beginSleep(DateTime time) {
+    if (_startSleepTime == null) {
+      _startSleepTime = time;
+    } else {
+      throw Exception('Should never happen!');
+    }
+  }
+
+  void endSleep(DateTime endSleepTime) {
+    for (var i = _startSleepTime.minute; i < endSleepTime.minute; i++) {
+      sleepMap.update(i, (i) => i + 1, ifAbsent: () => 1);
+    }
+    _startSleepTime = null;
+  }
+
+  int get sleepSum => sleepMap.values.fold(0, (p, e) => p + e);
+
+  int get mostSleepyMinute {
+    var maxValue = 0;
+    var maxMinute = 0;
+
+    sleepMap.forEach((minute, value) {
+      if (value > maxValue) {
+        maxMinute = minute;
+        maxValue = value;
+      }
+    });
+
+    return maxMinute;
+  }
+}
+
+int solveA(List<String> input) {
+  final guards = _parseInput(input);
+  final mostSleepyGuard =
+      guards.reduce((g1, g2) => g1.sleepSum > g2.sleepSum ? g1 : g2);
+
+  return mostSleepyGuard.id * mostSleepyGuard.mostSleepyMinute;
+}
+
+int solveB(List<String> input) {
+  final guards = _parseInput(input);
+
+  Guard maxGuard;
+  var maxSleep = 0;
+  var maxMinute = 0;
+
+  // Scan each possible minute
+  for (var i = 0; i < 60; i++) {
+    for (var guard in guards) {
+      final howOftenTheGuardSleepOnThisMinute = guard.sleepMap[i] ?? 0;
+
+      if (howOftenTheGuardSleepOnThisMinute > maxSleep) {
+        maxSleep = howOftenTheGuardSleepOnThisMinute;
+        maxGuard = guard;
+        maxMinute = i;
+      }
+    }
+  }
+
+  return maxGuard.id * maxMinute;
+}
+
+Iterable<Guard> _parseInput(List<String> input) {
+  final sortedInput = input.toList(growable: false)
+    ..sort((line1, line2) => _getTime(line1).compareTo(_getTime(line2)));
+
+  final exp = RegExp(r'#([0-9]+)');
+  final guardMap = <int, Guard>{};
+  Guard currentGuard;
+
+  for (var line in sortedInput) {
+    if (line.contains('Guard')) {
+      final id = int.parse(exp.firstMatch(line).group(1));
+      currentGuard = guardMap.putIfAbsent(id, () => Guard(id));
+    } else if (line.endsWith('falls asleep')) {
+      currentGuard.beginSleep(_getTime(line));
+    } else if (line.endsWith('wakes up')) {
+      currentGuard.endSleep(_getTime(line));
+    }
+  }
+
+  return guardMap.values;
+}
+
+// [1518-11-01 00:00] Guard #10 begins shift
+Map<String, DateTime> _cache = {};
+DateTime _getTime(String line) =>
+    _cache.putIfAbsent(line, () => DateTime.parse(line.substring(1, 17)));
---
+++ test/day04_test.dart
@@ -0,0 +1,48 @@
+// --- Day 4: Repose Record ---
+// https://adventofcode.com/2018/day/4
+
+import 'dart:io';
+import 'package:test/test.dart';
+import 'package:AdventOfCode2018/day04.dart';
+
+const String dataFilePath = 'test/data/day04.txt';
+
+const List<String> example = [
+  '[1518-11-01 00:00] Guard #10 begins shift',
+  '[1518-11-01 00:05] falls asleep',
+  '[1518-11-01 00:25] wakes up',
+  '[1518-11-01 00:30] falls asleep',
+  '[1518-11-01 00:55] wakes up',
+  '[1518-11-01 23:58] Guard #99 begins shift',
+  '[1518-11-02 00:40] falls asleep',
+  '[1518-11-02 00:50] wakes up',
+  '[1518-11-03 00:05] Guard #10 begins shift',
+  '[1518-11-03 00:24] falls asleep',
+  '[1518-11-03 00:29] wakes up',
+  '[1518-11-04 00:02] Guard #99 begins shift',
+  '[1518-11-04 00:36] falls asleep',
+  '[1518-11-04 00:46] wakes up',
+  '[1518-11-05 00:03] Guard #99 begins shift',
+  '[1518-11-05 00:45] falls asleep',
+  '[1518-11-05 00:55] wakes up',
+];
+
+void main() {
+  group('Part One', () {
+    test('Example 1', () {
+      expect(solveA(example), equals(240));
+    });
+    test('Solution', () {
+      expect(solveA(File(dataFilePath).readAsLinesSync()), equals(20859));
+    });
+  });
+
+  group('Part Two', () {
+    test('Example 1', () {
+      expect(solveB(example), equals(4455));
+    });
+    test('Solution', () {
+      expect(solveB(File(dataFilePath).readAsLinesSync()), equals(76576));
+    });
+  });
+}
