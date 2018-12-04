bb29bfacd75644e290de02ad4948eb9171fa5df0
Jacob Bang <julemand101@archlinux.dk>
2018-12-04 08:15:59 +0100

    Day03: Refactor test

--- test/day03_test.dart
+++ test/day03_test.dart
@@ -7,11 +7,16 @@ import 'package:AdventOfCode2018/day03.dart';
 
 const String dataFilePath = 'test/data/day03.txt';
 
+const List<String> example = [
+  '#1 @ 1,3: 4x4',
+  '#2 @ 3,1: 4x4',
+  '#3 @ 5,5: 2x2'
+];
+
 void main() {
   group('Part One', () {
     test('Example 1', () {
-      expect(solveA(['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']),
-          equals(4));
+      expect(solveA(example), equals(4));
     });
     test('Solution', () {
       expect(solveA(File(dataFilePath).readAsLinesSync()), equals(111935));
@@ -20,8 +25,7 @@ void main() {
 
   group('Part Two', () {
     test('Example 1', () {
-      expect(solveB(['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']),
-          equals(3));
+      expect(solveB(example), equals(3));
     });
     test('Solution', () {
       expect(solveB(File(dataFilePath).readAsLinesSync()), equals(650));
