96f6f0da7316875ab150578aaa57c2b7d58b1ded
Jacob Bang <julemand101@archlinux.dk>
2018-12-07 00:46:44 +0100

    Day06: Solved

---
+++ lib/day06.dart
@@ -0,0 +1,118 @@
+// --- Day 6: Chronal Coordinates ---
+// https://adventofcode.com/2018/day/6
+
+import 'dart:math';
+
+class Coordinate extends Point<int> {
+  const Coordinate(int x, int y) : super(x, y);
+
+  int manhattanDistanceByCoordinate(int x, int y) =>
+      (this.x - x).abs() + (this.y - y).abs();
+}
+
+class Grid {
+  final List<Coordinate> coordinates;
+
+  int maxX = 0;
+  int minX = 0;
+  int maxY = 0;
+  int minY = 0;
+
+  Grid(this.coordinates) {
+    for (var coordinate in coordinates) {
+      maxX = max(coordinate.x, maxX);
+      minX = min(coordinate.x, minX);
+
+      maxY = max(coordinate.y, maxY);
+      minY = min(coordinate.y, minY);
+    }
+  }
+
+  Coordinate closestPoint(int x, int y) {
+    Coordinate closestCoordinate;
+    int minDistance;
+
+    for (var coordinate in coordinates) {
+      final distance = coordinate.manhattanDistanceByCoordinate(x, y);
+
+      if (minDistance == null) {
+        closestCoordinate = coordinate;
+        minDistance = distance;
+      } else if (distance == minDistance) {
+        closestCoordinate = null;
+      } else if (distance < minDistance) {
+        closestCoordinate = coordinate;
+        minDistance = distance;
+      }
+    }
+
+    // null if two points has the same closest distance
+    return closestCoordinate;
+  }
+
+  int getDistanceSum(int x, int y) => coordinates.fold(
+      0, (prev, element) => prev + element.manhattanDistanceByCoordinate(x, y));
+
+  // Get all coordinates which does not reach the edge of the grid since these
+  // coordinates are going to expand into infinity
+  List<Coordinate> getValidCoordinates() {
+    final validCoordinates = coordinates.toList();
+
+    for (var x = minX; x <= maxX; x++) {
+      validCoordinates
+        ..remove(closestPoint(x, minY))
+        ..remove(closestPoint(x, maxY));
+    }
+
+    for (var y = minY; y <= maxY; y++) {
+      validCoordinates
+        ..remove(closestPoint(minX, y))
+        ..remove(closestPoint(maxX, y));
+    }
+
+    return validCoordinates;
+  }
+}
+
+int solveA(List<String> input) {
+  final grid = Grid(input.map(_parseToCoordinate).toList());
+
+  final validCoordinatesToSize = Map<Coordinate, int>.fromIterable(
+      grid.getValidCoordinates(),
+      key: (k) => k,
+      value: (_) => 0);
+
+  // We don't need to search the edge since all coordinates there are part of
+  // the edge has been removed from the list of valid coordinates
+  for (var x = grid.minX + 1; x < grid.maxX; x++) {
+    for (var y = grid.minY + 1; y < grid.maxY; y++) {
+      final coordinate = grid.closestPoint(x, y);
+
+      if (validCoordinatesToSize.containsKey(coordinate)) {
+        validCoordinatesToSize[coordinate]++;
+      }
+    }
+  }
+
+  return validCoordinatesToSize.values.fold(0, max);
+}
+
+int solveB(List<String> input, int totalDistanceLessThan) {
+  final grid = Grid(input.map(_parseToCoordinate).toList());
+  var areaSize = 0;
+
+  for (var x = grid.minX; x <= grid.maxX; x++) {
+    for (var y = grid.minY; y <= grid.maxY; y++) {
+      if (grid.getDistanceSum(x, y) < totalDistanceLessThan) {
+        areaSize++;
+      }
+    }
+  }
+  
+  return areaSize;
+}
+
+Coordinate _parseToCoordinate(String line) {
+  final parts = line.split(', ');
+  return Coordinate(int.parse(parts[0]), int.parse(parts[1]));
+}
---
+++ test/day06_test.dart
@@ -0,0 +1,31 @@
+// --- Day 6: Chronal Coordinates ---
+// https://adventofcode.com/2018/day/6
+
+import 'dart:io';
+import 'package:test/test.dart';
+import 'package:AdventOfCode2018/day06.dart';
+
+const String dataFilePath = 'test/data/day06.txt';
+
+const List<String> example = ['1, 1', '1, 6', '8, 3', '3, 4', '5, 5', '8, 9'];
+
+void main() {
+  group('Part One', () {
+    test('Example 1', () {
+      expect(solveA(example), equals(17));
+    });
+    test('Solution', () {
+      expect(solveA(File(dataFilePath).readAsLinesSync()), equals(3569));
+    });
+  });
+
+  group('Part Two', () {
+    test('Example 1', () {
+      expect(solveB(example, 32), equals(16));
+    });
+    test('Solution', () {
+      expect(
+          solveB(File(dataFilePath).readAsLinesSync(), 10000), equals(48978));
+    });
+  });
+}
