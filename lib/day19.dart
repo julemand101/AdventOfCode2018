28bb0bcedf190c73c560d59c10c266d22beea5a8
Jacob Bang <julemand101@archlinux.dk>
2019-08-25 20:53:42 +0200

    Day19a: Example works
    The input program takes forever so I need to analyze the logic and optimize the assembly program.

---
+++ lib/day19.dart
@@ -0,0 +1,89 @@
+// --- Day 19: Go With The Flow ---
+// https://adventofcode.com/2018/day/19
+
+class Register {
+  int value;
+  Register(this.value);
+
+  @override
+  String toString() => value.toString();
+}
+
+typedef Instruction = void Function(List<Register> r, int a, int b, int c);
+
+Map<String, Instruction> instructions = Map.unmodifiable(<String, Instruction>{
+  // Addition
+  'addr': (r, a, b, c) => r[c].value = r[a].value + r[b].value,
+  'addi': (r, a, b, c) => r[c].value = r[a].value + b,
+
+  // Multiplication
+  'mulr': (r, a, b, c) => r[c].value = r[a].value * r[b].value,
+  'muli': (r, a, b, c) => r[c].value = r[a].value * b,
+
+  // Bitwise AND
+  'banr': (r, a, b, c) => r[c].value = r[a].value & r[b].value,
+  'bani': (r, a, b, c) => r[c].value = r[a].value & b,
+
+  // Bitwise OR
+  'borr': (r, a, b, c) => r[c].value = r[a].value | r[b].value,
+  'bori': (r, a, b, c) => r[c].value = r[a].value | b,
+
+  // Assignment
+  'setr': (r, a, b, c) => r[c].value = r[a].value,
+  'seti': (r, a, b, c) => r[c].value = a,
+
+  // Greater-than testing
+  'gtir': (r, a, b, c) => r[c].value = (a > r[b].value) ? 1 : 0,
+  'gtri': (r, a, b, c) => r[c].value = (r[a].value > b) ? 1 : 0,
+  'gtrr': (r, a, b, c) => r[c].value = (r[a].value > r[b].value) ? 1 : 0,
+
+  // Equality testing
+  'eqir': (r, a, b, c) => r[c].value = (a == r[b].value) ? 1 : 0,
+  'eqri': (r, a, b, c) => r[c].value = (r[a].value == b) ? 1 : 0,
+  'eqrr': (r, a, b, c) => r[c].value = (r[a].value == r[b].value) ? 1 : 0,
+});
+
+class CodeLine {
+  final String instructionName;
+  final Instruction instruction;
+  final int a, b, c;
+
+  CodeLine(this.instructionName, this.a, this.b, this.c)
+      : instruction = instructions[instructionName] {
+    if (instruction == null) {
+      throw Exception('Could not find implementation of $instructionName');
+    }
+  }
+
+  void call(List<Register> r) => instruction(r, a, b, c);
+
+  @override
+  String toString() => '$instructionName $a $b $c';
+}
+
+int solveA(List<String> lines) {
+  final regEx = RegExp(r'(\w{4}) (\d+) (\d+) (\d+)');
+  final codeLines = <CodeLine>[];
+  final registers = List.generate(6, (_) => Register(0));
+  Register ip;
+
+  for (var line in lines) {
+    if (line.startsWith('#')) {
+      ip = registers[int.parse(line.split(' ')[1])];
+    } else {
+      final parsed = regEx.firstMatch(line);
+      final instructionName = parsed.group(1);
+      final a = int.parse(parsed.group(2));
+      final b = int.parse(parsed.group(3));
+      final c = int.parse(parsed.group(4));
+
+      codeLines.add(CodeLine(instructionName, a, b, c));
+    }
+  }
+
+  while (ip.value >= 0 && ip.value < codeLines.length) {
+    codeLines[ip.value++].call(registers);
+  }
+
+  return ip.value;
+}
---
+++ test/day19_test.dart
@@ -0,0 +1,67 @@
+// --- Day 19: Go With The Flow ---
+// https://adventofcode.com/2018/day/19
+
+import 'package:test/test.dart';
+import 'package:AdventOfCode2018/day19.dart';
+
+const example = [
+  '#ip 0',
+  'seti 5 0 1',
+  'seti 6 0 2',
+  'addi 0 1 0',
+  'addr 1 2 3',
+  'setr 1 0 0',
+  'seti 8 0 4',
+  'seti 9 0 5',
+];
+
+const input = [
+  '#ip 5',
+  'addi 5 16 5',
+  'seti 1 2 2',
+  'seti 1 0 4',
+  'mulr 2 4 3',
+  'eqrr 3 1 3',
+  'addr 3 5 5',
+  'addi 5 1 5',
+  'addr 2 0 0',
+  'addi 4 1 4',
+  'gtrr 4 1 3',
+  'addr 5 3 5',
+  'seti 2 4 5',
+  'addi 2 1 2',
+  'gtrr 2 1 3',
+  'addr 3 5 5',
+  'seti 1 1 5',
+  'mulr 5 5 5',
+  'addi 1 2 1',
+  'mulr 1 1 1',
+  'mulr 5 1 1',
+  'muli 1 11 1',
+  'addi 3 6 3',
+  'mulr 3 5 3',
+  'addi 3 15 3',
+  'addr 1 3 1',
+  'addr 5 0 5',
+  'seti 0 7 5',
+  'setr 5 8 3',
+  'mulr 3 5 3',
+  'addr 5 3 3',
+  'mulr 5 3 3',
+  'muli 3 14 3',
+  'mulr 3 5 3',
+  'addr 1 3 1',
+  'seti 0 0 0',
+  'seti 0 6 5',
+];
+
+void main() {
+  group('Part One', () {
+    test('Example 1', () {
+      expect(solveA(example), equals(7));
+    });
+    test('Solution', () {
+      expect(solveA(input), equals(-1));
+    }, skip: true);
+  });
+}
